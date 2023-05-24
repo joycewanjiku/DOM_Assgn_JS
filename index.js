71f1c833b24ce488ae8d605d1cd229659adc5348
student <joycemumbiiwanjiku@gmail.com>
2023-05-24 16:55:49 +0300

    changes_DOM

--- index.js
+++ index.js
@@ -12,22 +12,23 @@ document.getElementById('title').style.color = "green";
 // h3.innerHTML = h3.innerHTML.toUpperCase();
 // document.getElementById("h3").style.textTransform = "toUppercase";
 
-let h3 = document.getElementsByTagName("h3");
-for (let i = 0; i < h3.length; i++) {
-  h3[i].style.textTransform = "uppercase";
-}
+
+document.getElementById('fruits').style.textTransform='uppercase'
+document.getElementById("vegetables").style.textTransform="uppercase"
 
 // Add one more fruit to the fruits list
-let h1 =document.creatElement('h1');
-h3.innerHTML="Oranges";
-document,creatElement('h1');
-document.getElementById('container').appendChild(h1);
+let li =document.createElement('li');
+li.innerHTML="Oranges";
+document.createElement('li');
+document.getElementById('fruList').appendChild(li);
 
 
 // // Add one more vegetable to the vegetables list
-// let vegetablesList = document.getElementById("vegetables");
-// let  newVegetable = document.createElement("li");
-// newVegetable.appendChild(document.createTextNode("Broccoli"));
-// vegetablesList.appendChild(newVegetable);
+let list =document.createElement('li');
+list.innerHTML="Spinach";
+document.createElement('li');
+document.getElementById('vegList').appendChild(list);
+
+
 
 
